8d795db4e24d0868d679448a78be9509f4ab621e
ethanbonis <78179551+ethanbonis@users.noreply.github.com>
2021-03-18 14:32:30 +1300

    Quiz

--- Quiz.py
+++ Quiz.py
@@ -1,4 +1,5 @@
-# This is a quiz wow
+jm,# This is a quiz wow
+A = 6
 ask_name = True
 while ask_name == True:
     name = input("Whats your name ")
@@ -9,6 +10,23 @@ while ask_name == True:
         print("ok then", name)
 questions_theory = True
 while questions_theory == True:
-    input(int("How many questions do you think you will get right?")
-
+    try:
+        print("How many questions do you")
+        questions_right = int(input("think you will get right? "))
+        questions_theory = False
+    if questions_right <=A:
+    print("A")
+    except:
+        print("Answer needs to be a number ")
+    else:
+        print("ok")
+questions_1 = True
+while questions_1 == True:
+    try:
+        first_question = int(input("How many states are there in America "))
+        first_question = False
+    except:
+        print("Answer needs to be a number ")
+    else:
+        print("ok")
 
